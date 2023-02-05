f426a87987cbc419b7a6187cc15eac82a971bc7f
Mohammed <sajidcurious9@gmail.com>
2023-02-05 15:18:37 +0530

    making the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import maldivesVideo from "./assets/maldivesVideo.mp4";
+import { AiOutlineSearch } from "react-icons/Ai";
 
 const Hero = () => {
   return (
@@ -11,6 +12,21 @@ const Hero = () => {
         muted
         loop
       />
+      <div className="hero-color absolute w-full h-full top-0 left-0 bg-gray-900/70"></div>
+      <div className="hero-text absolute top-0 left-0 w-full h-full flex flex-col justify-center text-center text-white p-4">
+        <h1>First Class Travel</h1>
+        <h2>Top 1% Locations Worldwide</h2>
+        <form action="" className="bg-gray-100/90 flex">
+          <div className="input-text">
+            <input type="text" placeholder="Search Destinations" />
+          </div>
+          <div className="search-button">
+            <button>
+              <AiOutlineSearch />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
