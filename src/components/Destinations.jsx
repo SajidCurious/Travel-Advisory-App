3f18cc4a042fcad623ddc2feaebcbb6ce46cc847
Mohammed <sajidcurious9@gmail.com>
2023-02-05 15:36:49 +0530

    started building destinations page

---
+++ src/components/Destinations.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Destinations = () => {
+  return <div>Destinations</div>;
+};
+
+export default Destinations;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,14 +15,21 @@ const Hero = () => {
       <div className="hero-color absolute w-full h-full top-0 left-0 bg-gray-900/70"></div>
       <div className="hero-text absolute top-0 left-0 w-full h-full flex flex-col justify-center text-center text-white p-4">
         <h1>First Class Travel</h1>
-        <h2>Top 1% Locations Worldwide</h2>
-        <form action="" className="bg-gray-100/90 flex">
+        <h2 className="py-4">Top 1% Locations Worldwide</h2>
+        <form
+          action=""
+          className="bg-gray-100/90 flex justify-between items-center max-w-[700px] mx-auto w-full border p-1 rounded-md text-black"
+        >
           <div className="input-text">
-            <input type="text" placeholder="Search Destinations" />
+            <input
+              type="text"
+              placeholder="Search Destinations"
+              className="bg-transparent w-fit focus:outline-none"
+            />
           </div>
           <div className="search-button">
             <button>
-              <AiOutlineSearch />
+              <AiOutlineSearch size={20} />
             </button>
           </div>
         </form>
